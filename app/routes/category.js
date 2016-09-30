a3672427626185125bfc3963af0c24eff3497375
Flo M <root@tpflo>
2016-09-30 16:36:24 +0200

    #2 #4 finished view of main page, categories, post and keywords

--- app/routes/category.js
+++ app/routes/category.js
@@ -5,6 +5,7 @@ import RSVP from 'rsvp';
 export default Ember.Route.extend({
   model(params) {
     return RSVP.hash({
+      category: params.name,
       page : params.page,
       posts: this.get('store').query('post', { page: params.page, limit: 5,
         sort: 'created',orderasc:false, postStatus: 'PUBLIC', category: params.name}),
--- app/routes/keyword.js
+++ app/routes/keyword.js
@@ -5,6 +5,7 @@ import RSVP from 'rsvp';
 export default Ember.Route.extend({
   model(params) {
     return RSVP.hash({
+      keyword : params.keyword,
       page : params.page,
       posts: this.get('store').query('post', { page: params.page, limit: 5,
         sort: 'created',orderasc:false, postStatus: 'PUBLIC', keywords: params.keyword}),
